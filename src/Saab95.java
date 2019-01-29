b4998243cca02add238257f0b82578f06cd526dd
ntigymnasiet <linus.norberg.te16c@gmail.com>
2019-01-29 15:58:15 +0100

    flyttning
    flyttade till "car"

--- src/Saab95.java
+++ src/Saab95.java
@@ -4,10 +4,7 @@ public class Saab95 extends car{
 
 	public boolean turboOn;
 
-	public double enginePower; // Engine power of the car
-	public double currentSpeed; // The current speed of the car
-	public Color color; // Color of the car
-	public String modelName; // The car model name
+	
 
 	public Saab95() {
 		color = Color.BLACK;
--- src/Volvo740.java
+++ src/Volvo740.java
@@ -4,10 +4,7 @@ public class Volvo740 extends car{
 
 	public final static double trimFactor = 1.25;
 
-	public double enginePower; // Engine power of the car
-	public double currentSpeed; // The current speed of the car
-	public Color color; // Color of the car
-	public String modelName; // The car model name
+	
 
 	public Volvo740() {
 		color = Color.RED;

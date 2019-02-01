18a4ddba3c658da8b65847ac48b1bdb5af804a66
TheThingIs <markus-grahn@hotmail.com>
2019-02-01 11:58:52 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/linusgymnasiet/diglettfanclub""
    This reverts commit c48b78240d4897297735b8a394809c5ca2b9d9b6.

--- src/car.java
+++ src/car.java
@@ -1,4 +1,11 @@
+import javafx.scene.paint.Color;
+
 
 public class car {
+	
+	public double enginePower; // Engine power of the car
+	public double currentSpeed; // The current speed of the car
+	public Color color; // Color of the car
+	public String modelName; // The car model name
 
 }

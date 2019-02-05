6959079340da7d768f241835bed70c9a9e106cf0
TheThingIs <markus-grahn@hotmail.com>
2019-02-05 16:29:47 +0100

    lade till javadoc i saab

--- src/Saab95.java
+++ src/Saab95.java
@@ -3,7 +3,9 @@ import javafx.scene.paint.Color;
 public class Saab95 extends car{
 
 	private boolean turboOn;
-
+	/**
+	 * Creates a saab95
+	 */
 	public Saab95() {
 		color = Color.BLACK;
 		enginePower = 125;
@@ -11,11 +13,15 @@ public class Saab95 extends car{
 		modelName = "Saab95";
 		stopEngine();
 	}
-
+	/**
+	 * urns on the turbo
+	 */
 	public void setTurboOn() {
 		turboOn = true;
 	}
-
+	/**
+	 * Turns off the turbo
+	 */
 	public void setTurboOff() {
 		turboOn = false;
 	}

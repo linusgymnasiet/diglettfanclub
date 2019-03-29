769dc0b403fae582d5ecbb2e3dd45419d5c3ac88
rassii00 <42772672+rassii00@users.noreply.github.com>
2019-03-29 12:20:11 +0100

    PUSH PUSH
    PUSH PUSH

---
+++ src/controller/CarController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javafx.animation.AnimationTimer;
+import model.Vehicle;
+import view.CarPane;
+
+public class CarController {
+
+	private Vehicle model;
+
+	public CarController(CarPane cp) {
+
+		AnimationTimer at = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				if (model != null) {
+					model.move();
+					cp.move(model.getX(), model.getY());
+					cp.update();
+				}
+			}
+		};
+		at.start();
+	}
+
+	public void setModel(Vehicle v) {
+		model = v;
+	}
+
+	public void gas(double amount) {
+		model.gas(amount);
+	}
+
+}
